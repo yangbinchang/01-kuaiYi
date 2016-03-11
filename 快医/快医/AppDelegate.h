b949765aee56efa9ad09c3100fd915c92f735de0
yang <yang@yangdemac.local>
2016-03-11 18:57:49 +0800

    创建项目

---
+++ 快医/快医/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  快医
+//
+//  Created by yang on 16/3/11.
+//  Copyright © 2016年 yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 快医/快医/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  快医
+//
+//  Created by yang on 16/3/11.
+//  Copyright © 2016年 yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
